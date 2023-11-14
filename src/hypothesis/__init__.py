c9898c6e537524bfe37bcfd63291b34da284f6bf
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-11-14 18:57:09 +0200

    delete garbage files

--- src/hypothesis/__init__.py
+++
@@ -1,6 +0,0 @@
-'''Deprecated Module'''
-class HypothesisAlgo():
-    
-    def __init__(self,interior_points,border_points):
-        self.interior_points = interior_points
-        self.border_points = border_points
