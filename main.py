38531d22ceac3231d390148153f01ffcc251fd47
Yaniv Ohayon <yanivohayon8@gmail.com>
2024-06-26 11:50:33 +0300

    deleting failed sampling folder

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 import random
 import uuid
 from datetime import datetime
+import shutil
 
 
-def create_puzzle():
-    dirs_to_create = [args.puzzles_dst_folder,os.path.join(args.puzzles_dst_folder,"results")]
+def create_puzzle(puzzles_dst_folder):
+    dirs_to_create = [puzzles_dst_folder,os.path.join(puzzles_dst_folder,"results")]
 
     for _dir in dirs_to_create:
         if not os.path.exists(_dir):
@@ -20,11 +21,11 @@ def create_puzzle():
         board = Board(file_path=args.sampling_src_file)
     elif args.sampling_img_path != "":
         num_sampled_points = int(args.number_sampled_points)
-        interior_points,convex_hull_points,_ = sampler.sample_image(num_sampled_points,args.sampling_img_path,output_dir=args.puzzles_dst_folder)
+        interior_points,convex_hull_points,_ = sampler.sample_image(num_sampled_points,args.sampling_img_path,output_dir=puzzles_dst_folder)
         board = Board(interior_points=interior_points,convex_hull_points=convex_hull_points)
     elif not args.circle_num_ch is None:
         interior_points,convex_hull_points,_ = sampler.sample_points_on_circle(args.circle_num_ch,args.circle_num_interior,
-                                                                               args.circle_radius,output_dir=args.puzzles_dst_folder)
+                                                                               args.circle_radius,output_dir=puzzles_dst_folder)
         board = Board(interior_points=interior_points,convex_hull_points=convex_hull_points)
     else:
         interior_points,convex_hull_points, out_path = sampler.sampler_manual(output_dir=args.manual_sampling_out_dir)
@@ -32,7 +33,7 @@ def create_puzzle():
         board = Board(interior_points=interior_points,convex_hull_points=convex_hull_points)
 
     try:
-        creator = AllPartitionsCreator(board,args.puzzles_dst_folder,is_save_partitions_figures=args.save_partitions_figures)
+        creator = AllPartitionsCreator(board,puzzles_dst_folder,is_save_partitions_figures=args.save_partitions_figures)
         creator.run()
         
         plt.close("all")
@@ -61,11 +62,19 @@ if __name__ == "__main__":
     parser.add_argument("--circle_radius",default=5000,type=float)
     args = parser.parse_args()
 
-    if args.dst_folder_uuid:
+    for creation_i in range(args.num_puzzles):
+        puzzles_dst_folder = args.puzzles_dst_folder
+
+        # if args.dst_folder_uuid:
         current_dateTime = datetime.now()
         random_dir_name = str(current_dateTime).split(".")[0]
         random_dir_name = random_dir_name.replace(":","-")
         random_dir_name = random_dir_name + args.postfix_dst_folder_uuid
-        args.puzzles_dst_folder = os.path.join(args.puzzles_dst_folder,random_dir_name)
+        puzzles_dst_folder = os.path.join(puzzles_dst_folder,random_dir_name)
 
-    create_puzzle()
+        try:
+            print(f"Start to create puzzle to {puzzles_dst_folder} ({creation_i+1}/{args.num_puzzles})")
+            create_puzzle(puzzles_dst_folder)
+        except Exception as e:
+            print(f"Error:{e}")
+            shutil.rmtree(puzzles_dst_folder,ignore_errors=True) # be carefull not to delete the entire dataset!
